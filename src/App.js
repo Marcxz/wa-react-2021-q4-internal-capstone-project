2c425f3416de75cbff3f55161301eb7038037c56
Miguel Rodriguez <miguel.rodriguez@wizeline.com>
2021-11-21 17:13:47 -0600

    feat: Add footer to the project

--- src/App.js
+++ src/App.js
@@ -2,30 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import { useFeaturedBanners } from './utils/hooks/useFeaturedBanners';
 import Header from './header/Header'
+import Footer from './footer/Footer'
 
 function App() {
   const { data, isLoading } = useFeaturedBanners();
   console.log(data, isLoading);
 
   return (
-    <div>
-    <Header />
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Header />
+      <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+      </div>
+      <Footer />
+      
     </div>
   );
 }
