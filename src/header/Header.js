367958a26134cb40ed0592af1a36f9f7add0f1e1
Miguel Rodriguez <miguel.rodriguez@wizeline.com>
2021-12-02 16:42:12 -0600

    Add link between searchbar and products. Rest Pagination and netify

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,23 +1,17 @@
 import "./Header.css"
-import React, {useState} from 'react';
+import React from 'react';
 import { Link } from "react-router-dom"
 
 export default function Header() {
-    const [criterio, setCriterio] = useState("")
-    const handleKeyPress = (event) => {
-        setCriterio(document.getElementById('criterio').value)
-    }
-
-    
     return (
             <div className="container">
                 <Link to="/home">
                     <img alt="logo_image" src="assets/logo.png" /> 
                 </Link>
-                <input id="criterio" className="searchInput" placeholder="Teclee los elementos a buscar" type="text" onKeyUpCapture={handleKeyPress}/>
-                <Link className="linkSearch" to={`/search?q=${criterio}`}>
-                    <button className="btn">Buscar Elementos</button>
-                </Link>
+                <form method="get" action="/search">
+                    <input name="q" className="searchInput"  placeholder="Teclee los elementos a buscar" type="text"/>
+                    <button className="btnSearch" type="submit">Buscar Elementos</button>
+                </form>
             </div>
     )
 }
--- src/home/searchProduct/SearchProduct.js
+++ src/home/searchProduct/SearchProduct.js
@@ -10,59 +10,71 @@ function SearchProduct() {
             const search = window.location.search;
             const params = new URLSearchParams(search);
             // here we will filter the products
-            if (criterio === "" || params.get('q') !== criterio) {    
-                setCriterio(params.get('q'))            
-                setProducts(featuredProducts.results)
+            if ((criterio === "" || params.get('q') !== criterio) && params.get('q').length > 0) {    
+                setCriterio(params.get('q'))
+                let features = []
+                featuredProducts.results.map((p) => {
+                    if (p.data.name.toLowerCase().indexOf(params.get('q').toLowerCase()) >= 0) {
+                        features.push(p)
+                    }
+                })
+                setProducts(features)
             }
     }
-    console.log(criterio)
     useEffect(() => {
-        console.log("useeffect")
         queryProducts()
     })
-    return (
-        <div className="containerSearchProduct">
-            <h3>Resultados encontrados con el criterio  ({products.length}): {criterio}</h3>
-            {products && products.map((product, index) => 
-                <div className="cardSearchProduct">
-                <img src={product.data.mainimage.url} alt={product.data.mainimage.alt} />
-                <div className="containerDataSearchProduct">
-                    <span className="titleProduct">{product.data.name}</span>
-                    <div className="containerRowPD">
-                            <div className="containerShortDescriptionSearchProduct">
-                                <div className="title"></div>
-                                <span className="description">{product.data.short_description}</span>
+    if (products.length === 0) {
+        return (
+            <div className="containerSearchProduct">
+                <h1>No hubo resultados para con el criterio especificado: {criterio}</h1>
+            </div>
+        )    
+    } else {
+        return (
+            <div className="containerSearchProduct">
+                <h3>Resultados encontrados con el criterio  ({products.length}): {criterio}</h3>
+                {products && products.map((product, index) => 
+                    <div key={index} className="cardSearchProduct">
+                    <img src={product.data.mainimage.url} alt={product.data.mainimage.alt} />
+                    <div className="containerDataSearchProduct">
+                        <span className="titleProduct">{product.data.name}</span>
+                        <div className="containerRowPD">
+                                <div className="containerShortDescriptionSearchProduct">
+                                    <div className="title"></div>
+                                    <span className="description">{product.data.short_description}</span>
+                                </div>
                             </div>
-                        </div>
-                    <div className="containerRowPD">
-                        <div className="containerSubDataSearchProduct">
-                            <span className="title">Categoria:</span>
-                            <span className="description">{product.data.category.slug}</span>
-                        </div>
-                    </div>
-                    <div className="containerRowPD">
-                        <div className="containerSubDataSearchProduct">
-                            <span className="title">Precio:</span>
-                            <span className="description">$ {product.data.price}</span>
-                        </div>
-                    </div>
                         <div className="containerRowPD">
                             <div className="containerSubDataSearchProduct">
-                                <span className="title">Stock:</span>
-                                <span className="description">{product.data.stock}</span>
+                                <span className="title">Categoria:</span>
+                                <span className="description">{product.data.category.slug}</span>
                             </div>
                         </div>
-                        
                         <div className="containerRowPD">
                             <div className="containerSubDataSearchProduct">
-                                <button>Agregar al carrito</button>
+                                <span className="title">Precio:</span>
+                                <span className="description">$ {product.data.price}</span>
                             </div>
                         </div>
-                </div>
-                </div>
-            )}
-        </div>
-    )
+                            <div className="containerRowPD">
+                                <div className="containerSubDataSearchProduct">
+                                    <span className="title">Stock:</span>
+                                    <span className="description">{product.data.stock}</span>
+                                </div>
+                            </div>
+                            
+                            <div className="containerRowPD">
+                                <div className="containerSubDataSearchProduct">
+                                    <button>Agregar al carrito</button>
+                                </div>
+                            </div>
+                    </div>
+                    </div>
+                )}
+            </div>
+        )
+    }
 }
 
 export default SearchProduct;
