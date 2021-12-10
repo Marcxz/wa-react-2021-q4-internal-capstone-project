99907e2df0d0bfd6655a773dbfbe4c3cad908d58
Miguel Rodriguez <miguel.rodriguez@wizeline.com>
2021-12-10 01:13:54 -0600

    fixing search item to work in netify

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,17 +1,21 @@
 import "./Header.css"
 import React from 'react';
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import ShoppingCartIcon from "../home/shoppingCartIcon/ShoppingCartIcon";
 export default function Header() {
-    return (
+    const navigate = useNavigate();
+    const searchProduct = () => {
+        navigate(`search?q=${document.getElementById('q').value}`)
+    }
+     return (
             <div className="container">
                 <Link to="/home">
                     <img alt="logo_image" src="assets/logo.png" /> 
                 </Link>
-                <form method="get" action="/search">
-                    <input name="q" className="searchInput"  placeholder="Teclee los elementos a buscar" type="text"/>
-                    <button className="btnSearch" type="submit">Buscar Elementos</button>
-                </form>
+                <div className="containerSearch">
+                    <input id="q" className="searchInput"  placeholder="Teclee los elementos a buscar" type="text"/>
+                    <button className="btnSearch" onClick={()=>{searchProduct()}}>Buscar Elementos</button>
+                </div>
                <ShoppingCartIcon />
             </div>
     )
